4f21eb20e9035f2fd54cd714363cc235fefcbeab
Fernando Suzin <suzin.dev@gmail.com>
2024-06-27 21:31:25 -0300

    Implementado layout para login

--- z-front/src/app/page.tsx
+++ z-front/src/app/page.tsx
@@ -1,7 +1,9 @@
+import { CreateAccount } from "@/components/create-account";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Olá</h1>
+    <main className="grid grid-cols-1 max-w-[500px] m-auto py-20">
+      <CreateAccount />
     </main>
   );
 }
---
+++ z-front/src/components/create-account/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import { Icons } from "../icons";
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import ZImage from "../../images/z.png";
+
+export function CreateAccount() {
+  return (
+    <Card className="w-full h-max p-2 border-muted-foreground">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl uppercase">Bem Vindo</CardTitle>
+        <CardDescription>Use sua conta do Github para entrar</CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className="grid grid-cols-1 gap-6">
+          <Button variant="outline">
+            <Icons.gitHub className="mr-2 h-6 w-6" />
+            Github
+          </Button>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Image
+          src={ZImage}
+          alt="Bem vindo ao Z!"
+          className="m-auto"
+          width={375}
+          height={375}
+        />
+      </CardFooter>
+    </Card>
+  );
+}
